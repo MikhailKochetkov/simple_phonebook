b2d1877225efd6bd2aa5b28e215fca686c2b6320
MikhailKochetkov <consultant.dax@gmail.com>
2023-11-03 19:47:29 +0300

    Refactoring

--- helpers.py
+++ helpers.py
@@ -48,17 +48,6 @@ def get_max_id():
         return 0
 
 
-## TODO: Remove input_data_dict
-def input_data_dict(n: int, m: int):
-    data = {}
-    fields = get_columns_headers(FILE)
-    for field in fields[n:m]:
-        value = input(f'please enter {field.replace("_", " ")}: ')
-        if value:
-            data[field] = value
-    return data
-
-
 def input_data(fields: list, start: int | None, stop: int | None) -> str:
     for field in fields[start:stop]:
         value = input(f'please enter {field.replace("_", " ")}: ')
--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from handlers import (
 from helpers import (
     get_columns_headers,
     get_search_results,
-    input_data_dict,
     input_data)
 from settings import FILE
 
@@ -41,18 +40,25 @@ def main():
         print(f'search result:\n{search(result)}')
     if choice == 'e':
         print('whose data do you want to update?')
-        ed = input_data_dict(1, len(HEADERS[:4]))
-        count = len(get_search_results(ed))
+        e_result = {}
+        e_generator = input_data(fields, start=1, stop=4)
+        for e_data in e_generator:
+            if any(e_data.values()):
+                e_result.update({k: v for k, v in e_data.items() if v})
+        count = len(get_search_results(e_result))
         print(f'found records to your request: {count}')
-        print(search(ed))
+        print(search(e_result))
         if count > 1:
             rec_id = input('which record do you want to update?: ')
         else:
-            rec_id = get_search_results(ed)[0]['id']
+            rec_id = get_search_results(e_result)[0]['id']
         print('what data do you want to change?')
-        fields_to_change = input_data_dict(4, len(HEADERS))
-        print(edit_record(rec_id, fields_to_change))
-        print(search(ed))
+        fields_to_change_result = {}
+        fields_to_change = input_data(fields, start=4, stop=None)
+        for fc in fields_to_change:
+            if any(fc.values()):
+                fields_to_change_result.update({k: v for k, v in fc.items() if v})
+        print(edit_record(rec_id, fields_to_change_result))
     if choice == 'o':
         print(f'result:\n{read_file_data()}')
     if choice == 'a':
